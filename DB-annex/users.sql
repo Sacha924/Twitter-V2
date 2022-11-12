a7814a5c7d3e89e5cb1bd55d20b96976c3e4ab55
Sacha924 <24448@student.dorset-college.ie>
2022-11-12 23:32:10 +0100

    adding table to handle post info

--- DB-annex/users.sql
+++ DB-annex/users.sql
@@ -17,9 +17,25 @@ CREATE TABLE `users` (
   `posts` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE `posts` (
+  `post_id` int(11) NOT NULL,
+  `user_ID` int(11) NOT NULL,
+  `post_content` varchar(255) NOT NULL,
+  `upload_image` varchar(255) NOT NULL,
+  `post_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`post_id`);
+
+ALTER TABLE `posts`
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
 ALTER TABLE `users`
   ADD PRIMARY KEY (`user_ID`);
 
 ALTER TABLE `users`
   MODIFY `user_ID` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
+-- The COMMIT command is the transactional command used to save changes invoked by a transaction to the database. 
