e3eda9180fd544dadfc3da3ea0b1fec6bc17b510
AntoineSirot <sirot_antoine@bbox.fr>
2022-12-11 19:24:55 +0100

    Server deployed

--- functions/functions.php
+++ functions/functions.php
@@ -1,6 +1,13 @@
 <?php
 
-$con = mysqli_connect("localhost","root","","web_dev_fin_a4") or die("Connection was not established");
+$servername = "localhost";
+$username = "grp12tlen89k81w";
+$password = "A37kmRIKUa"; 
+$database = "mygrp1";
+
+
+// Create connection
+$con = mysqli_connect($servername, $username, $password, $database);
 
 //function for inserting post
 function insertPost(){
--- includes/connection.php
+++ includes/connection.php
@@ -1,3 +1,10 @@
 <?php
-$con = mysqli_connect("localhost","root","","web_dev_fin_a4");
+$servername = "localhost";
+$username = "grp12tlen89k81w";
+$password = "A37kmRIKUa"; 
+$database = "mygrp1";
+
+
+// Create connection
+$con = mysqli_connect($servername, $username, $password, $database);
 ?>
